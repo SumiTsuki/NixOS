f3691cc7bd00ea919b6a1c94e22679b19956de6d
SumiTsuki <InkLuna@outlook.com>
2026-03-19 02:58:26 +0800

    NixOS

--- home-manager/emacs/emacs/lisp/init-ui.el
+++ home-manager/emacs/emacs/lisp/init-ui.el
@@ -10,10 +10,17 @@
 (use-package dashboard
   :config
   (dashboard-setup-startup-hook)
-  (setq dashboard-startup-banner 'official)
+  (setq dashboard-startup-banner "Luna.txt")
+  (setq dashboard-banner-logo-title "Welcome to Luna's Emacs.")
+  (setq dashboard-center-content t)
+  (setq dashboard-vertically-center-content t)
   (setq dashboard-items '((recents . 5)
+						  (bookmarks . 5)
 						  (projects . 5)
-						  (bookmarks . 5)))
+						  (agenda . 5)
+						  (registers . 5)))
+  (setq dashboard-display-icons-p t)
+  (setq dashboard-icon-type 'nerd-icons)
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t))
 
