4ffc5d7b6bc09bc93aba3189cac889f579db0e0d
SumiTsuki <InkLuna@outlook.com>
2026-03-20 14:50:51 +0800

    NixOS

--- modules/base/network.nix
+++ modules/base/network.nix
@@ -2,4 +2,17 @@
 
 {
 	programs.clash-verge.enable = true;
+	networking.proxy = {
+    default = "http://127.0.0.1:7897";
+    noProxy = "127.0.0.1,localhost,::1";
+    # 如果需要 HTTPS 单独配置
+    # httpsProxy = "http://127.0.0.1:7897";
+  };
+  
+  # 方法2：同时保留环境变量（双保险）
+  environment.sessionVariables = {
+    HTTP_PROXY = "http://127.0.0.1:7897";
+    HTTPS_PROXY = "http://127.0.0.1:7897";
+    NO_PROXY = "127.0.0.1,localhost,::1";
+  };
 }
