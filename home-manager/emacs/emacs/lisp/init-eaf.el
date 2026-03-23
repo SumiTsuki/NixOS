04e2afb96ef6517910bf2c26cca61c2ccc918074
SumiTsuki <InkLuna@outlook.com>
2026-03-23 18:12:12 +0800

    NixOS

--- home-manager/emacs/emacs/lisp/init-eaf.el
+++ home-manager/emacs/emacs/lisp/init-eaf.el
@@ -1,21 +1,9 @@
 (use-package eaf
-  :straight (eaf
-			 :type git
-			 :host github
-			 :repo "emacs-eaf/emacs-application-framework"
-			 :submodules t
-			 :build nil)
+  :load-path "~/.config/emacs/site-lisp/emacs-application-framework"
   :custom
-  (eaf-python-command "python3")
-  (eaf-config-location (expand-file-name "eaf/" user-emacs-directory))
   (eaf-browser-continue-where-left-off t)
   (eaf-browser-enable-adblocker t)
   (browse-url-browser-function 'eaf-open-browser)
-  :init
-  (let ((repo-path (expand-file-name "straight/repos/emacs-application-framework" user-emacs-directory)))
-    (add-to-list 'load-path repo-path)
-    (add-to-list 'load-path (expand-file-name "app/browser" repo-path))
-    (add-to-list 'load-path (expand-file-name "app/pdf-viewer" repo-path)))
   :config
   (require 'eaf-browser)
   (require 'eaf-pdf-viewer)
@@ -24,5 +12,4 @@
   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
   (eaf-bind-key nil "M-q" eaf-browser-keybinding))
-
 (provide 'init-eaf)
