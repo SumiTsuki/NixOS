47d20debe327213cd01806d319e5f2f010e1f229
SumiTsuki <InkLuna@outlook.com>
2026-03-23 17:54:08 +0800

    NixOS

--- home-manager/emacs/emacs/lisp/init-eaf.el
+++ home-manager/emacs/emacs/lisp/init-eaf.el
@@ -11,12 +11,12 @@
   (eaf-browser-enable-adblocker t)
   (browse-url-browser-function 'eaf-open-browser)
   :config
+  (require 'eaf-browser)
+  (require 'eaf-pdf-viewer)
   (defalias 'browse-web #'eaf-open-browser)
   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-  (eaf-bind-key nil "M-q" eaf-browser-keybinding)
-  (require 'eaf-browser)
-  (require 'eaf-pdf-viewer))
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding))
 
 (provide 'init-eaf)
